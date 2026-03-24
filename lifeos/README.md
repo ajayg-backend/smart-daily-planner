50124020d4f836134c48ea67c1bc3d909607b7d8
ajay shivaji gavali <ajayshivajigavali@ajays-MacBook-Pro.local>
2026-03-24 18:05:04 +0530

    fixed folder structure

---
+++ lifeos/README.md
@@ -0,0 +1,20 @@
+# 🧠 Smart Daily Planner
+
+A web app built using Flask, HTML, CSS, and JavaScript.
+
+## 🚀 Features
+•⁠  ⁠Add tasks with time
+•⁠  ⁠Popup alert system
+•⁠  ⁠Sound notification 🔔
+•⁠  ⁠Delete tasks
+
+## 🛠 Tech Used
+•⁠  ⁠Python (Flask)
+•⁠  ⁠HTML
+•⁠  ⁠CSS
+•⁠  ⁠JavaScript
+
+## ▶️ Run Project
+
+pip install flask  
+python app.py
---
+++ lifeos/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/planner", methods=["GET", "POST"])
+def planner():
+
+    if request.method == "POST":
+
+
+        time = request.form.get("time")
+        task = request.form.get("task")
+        day = request.form.get("day")
+
+        tasks.append({
+            "day": day,
+            "time": time,
+            "task": task
+        })
+
+    return render_template("planner.html", tasks=tasks)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    if index < len(tasks):
+        tasks.pop(index)
+    return redirect("/planner")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("lifeos.db")
+        c = conn.cursor()
+
+        c.execute("INSERT INTO users (username,password) VALUES (?,?)",(username,password))
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/login")
+    
+    return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("lifeos.db")
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username,password))
+                  
+        user = c.fetchone()
+
+        conn.close()
+
+        if user:
+            return redirect("/")
+        else:
+            return "Login Failed"
+        
+    return render_template("login.html")
+
+    
+def create_db():
+    conn = sqlite3.connect("lifeos.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+    
+create_db()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
